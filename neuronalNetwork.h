f6c418a80f4a4ce7c84eb19c0992369506e12263
jaquer <ivanrodriguezferrandez@gmail.com>
2017-01-24 23:55:09 +0100

    starting files

---
+++ neuronalNetwork.h
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <ctime>
+
+
+class neuron;
+class layer;
+class neuronalNetwork{
+public:
+
+	~neuronalNetwork();
+
+
+private:
+	neuronalNetwork();
+
+
+};
+class layer{
+public:
+	~layer();
+
+
+private:
+}
+class neuron{
+public:
+	~neuron();
+
+private:
+
+};
---
+++ neuronalNetworkFunctions.h
@@ -0,0 +1,7 @@
+class neuronalNetworkFunctions
+{
+public:
+	neuronalNetworkFunctions();
+	~neuronalNetworkFunctions();
+	
+};
